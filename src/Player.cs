8dbd19002f66d1c5c57a31ea28cbb64b9046d4b2
QW3RT2U10PU <115784893+QW3RT2U10PU@users.noreply.github.com>
2026-01-09 20:24:58 +0100

    basic player movement

---
+++ src/Player.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	[Export]
+	public float Speed = 300.0f;
+
+	[Export]
+	public float JumpStrength = 400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("move_left", "move_right", "jump", "move_down");
+		velocity.X = direction.X * Speed;
+		if (direction.Y < 0 && IsOnFloor())
+		{
+			velocity.Y = -JumpStrength;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
