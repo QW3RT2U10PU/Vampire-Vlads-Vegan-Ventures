ced6c5969476f305335ec9482f0e0de707241558
justhlt <apfel493@gmail.com>
2026-01-10 16:45:58 +0100

    room2

--- src/Player.cs
+++ src/Player.cs
@@ -7,24 +7,64 @@ public partial class Player : CharacterBody2D
 
 	[Export]
 	public float Speed {get; set;} = 300.0f;
+	
+	[Export]
+	public float AirDrag {get; set;} = 1.7f;
+	
+	[Export]
+	public float DashSpeed {get; set;} = 5f;
 
 	[Export]
 	public float JumpStrength {get; set;} = 400.0f;
 
+	private enum DashState {ACTIVE = 4, COOLDOWN = 1, NEEDS_FLOOR = 2}
+	private DashState dashState = 0;
+
 	public override void _PhysicsProcess(double delta)
 	{
+		GD.Print(dashState);
+		Timer dashTimer = GetNode<Timer>("dashTimer");
+		Timer dashCooldown = GetNode<Timer>("dashCooldown");
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
-		if (!IsOnFloor())
+		Vector2 direction = Input.GetVector("move_left", "move_right", "jump", "move_down");
+		velocity.X = direction.X * Speed;
+		
+		if(Input.IsActionJustPressed("dash") && dashState == 0) {
+			dashTimer.Start();
+			dashCooldown.Start();
+			dashState = DashState.ACTIVE;
+		}
+
+		if(dashState == DashState.ACTIVE)
+		{
+			velocity.X *= DashSpeed;
+		}
+		
+		if (IsOnFloor())
+		{
+			if (dashState != DashState.ACTIVE) dashState &= ~DashState.NEEDS_FLOOR;
+		}
+		else if (dashState != DashState.ACTIVE)
 		{
 			velocity += GetGravity() * (float)delta;
+			velocity.X /= AirDrag;
 		}
+		else
+		{
+			velocity.Y = 0;
+		}
+			
+		
+
+		
+
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("move_left", "move_right", "jump", "move_down");
-		velocity.X = direction.X * Speed;
+		
+		
 		if (direction.Y < 0 && IsOnFloor())
 		{
 			velocity.Y = -JumpStrength;
@@ -32,5 +72,14 @@ public partial class Player : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+	public void DashExpired()
+	{
+		dashState = DashState.COOLDOWN | DashState.NEEDS_FLOOR;
+	}
+
+	public void DashCooldownExpired()
+	{
+		dashState &= ~DashState.COOLDOWN; 
 	}
 }
